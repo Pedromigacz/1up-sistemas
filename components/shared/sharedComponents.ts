a8dd72bd896a9cd9eac01f5f94f0df13bd5f2b25
Pedro H G Migacz <pedromigacz@gmail.com>
2023-05-18 13:57:19 -0300

    add genericSection components

---
+++ components/shared/sharedComponents.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const GenericInnerContainer = styled.div`
+  width: 100%;
+  max-width: var(--container-width);
+  padding: var(--container-padding);
+  margin: 0 auto;
+`;
+
+const GenericSection = styled.section`
+  width: 100%;
+  display: inline-grid;
+`;
