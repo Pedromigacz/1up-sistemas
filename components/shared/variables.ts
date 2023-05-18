2e74b972a9f3511b6c994800841a95742cd3ab38
Pedro H G Migacz <pedromigacz@gmail.com>
2023-05-18 13:56:48 -0300

    add css color variables

---
+++ components/shared/variables.ts
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+
+const CssVariables = createGlobalStyle`
+:root {
+    /* COLORS*/
+    --background-main: #0A0A0A;
+    --background-2: #161616;
+
+    --accent-cyan: #57FFDC;
+    --accent-blue: #2589BD;
+
+    --text-main: #ffffff;
+    --text-2: #B1B1B1;
+
+    --text-negative-main: #101D42;
+
+    /* CONTAINER */
+
+    --container-width: 1544px;
+    --container-padding: 0 32px;
+    --rounded: 20px;
+
+    /* FONTS */
+
+    /* --font-title: 'Sora', sans-serif;
+    --font-body: 'Inter', sans-serif; */
+
+    /* @media(min-width: 700px) {
+      --container-padding: 0 16px;
+    } */
+  }
+`;
+
+export default CssVariables;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import type { AppProps } from 'next/app';
 import GlobalStyles from '@/components/shared/globaStyles';
+import CssVariables from '@/components/shared/variables';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <CssVariables />
       <GlobalStyles />
       <Component {...pageProps} />
     </>
