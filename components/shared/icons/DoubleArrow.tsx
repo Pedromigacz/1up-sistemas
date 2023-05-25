f6a5b9dceceb987459b771430510508972420b2b
Gustavo G Migacz <gustavomigacz@gmail.com>
2023-05-25 17:45:18 -0300

    add svgs and images

---
+++ components/shared/icons/DoubleArrow.tsx
@@ -0,0 +1,18 @@
+const DoubleArroIcon = (): JSX.Element => {
+  return (
+    <svg
+      width='15'
+      height='18'
+      viewBox='0 0 15 18'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M13.2375 0.258789L15 2.07165L7.5 9.78593L0 2.07165L1.7625 0.258789L7.5 6.14736L13.2375 0.258789ZM13.2375 7.97307L15 9.78593L7.5 17.5002L0 9.78593L1.7625 7.97307L7.5 13.8616L13.2375 7.97307Z'
+        fill='#101D42'
+      />
+    </svg>
+  );
+};
+
+export default DoubleArroIcon;
